a9b5ef379ad3cd21540d4da687ce212ceee7d40f
jayparmar <jayparmar181999@gmail.com>
2023-04-19 09:49:43 +0530

    s

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const root = ReactDOM.createRoot(document.getElementById("root"));
+var page=(
+    <div className="container">
+        <div className="row">
+            <div className="col-4">
+                <h1>class topic</h1>
+                <ul className="list-group">
+                    <li className="list-group-item">HTML</li>
+                    <li className="list-group-item">css</li>
+                    <li className="list-group-item">bootstrap</li>
+                    <li className="list-group-item">java scrit</li>
+                    <li className="list-group-item">react</li>
+                </ul>
+
+            </div>
+        </div>
+    </div>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(page);
