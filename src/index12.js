f1a5313efa32f66bf78b207f34dcde1697c0a3be
jayparmar <jayparmar181999@gmail.com>
2023-05-10 21:55:41 +0530

    update

--- src/index12.js
+++ src/index12.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import AppLifeCycle from './AppLifeCycle'
+import Team from './team';
+import IPL from './IPL';
 var root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<AppLifeCycle />);
+root.render(<IPL />);
